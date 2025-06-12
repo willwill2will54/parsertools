2a354892a1104528f7412d08e4cb094a6295d617
HolographicTripwire <josephsmagen@gmail.com>
2025-06-12 08:55:24 +0100

    Re-implement lazy_parser to allow closures as well as function pointers

--- src/lazy_parser.rs
+++ src/lazy_parser.rs
@@ -1,40 +1,31 @@
-use std::sync::LazyLock;
+use crate::{AstBounds, LeftRecursionCheck, ParseOutput, Parser, ParserInner, TokenBounds};
 
-use crate::AstBounds;
-use crate::LeftRecursionCheck;
-use crate::Parser;
-use crate::TokenBounds;
-
-use super::ParseOutput;
-
-use super::ParserInner;
-
-pub(super) struct LazyParser<'a, T: TokenBounds, A: AstBounds> {
-    pub(super) inner: LazyLock<Parser<'a, T, A>>,
+pub(super) struct LazyParser<'a, T: TokenBounds, A: AstBounds, F: Send + Sync + Fn() -> Parser<'a,T,A>> {
+    pub(super) inner: F,
 }
 
-impl<T: TokenBounds, A: AstBounds> LazyParser<'_, T, A> {}
+impl<'a, T: TokenBounds, A: AstBounds, F: Send + Sync + Fn() -> Parser<'a,T,A>> LazyParser<'a, T, A, F> {}
 
-impl<T: TokenBounds, A: AstBounds> ParserInner for LazyParser<'_, T, A> {
+impl<'a, T: TokenBounds, A: AstBounds, F: Send + Sync + Fn() -> Parser<'a,T,A>> ParserInner for LazyParser<'a, T, A, F> {
     type Token = T;
     type Ast = A;
 
-    fn parse<'a>(&self, tokens: &'a [Self::Token]) -> ParseOutput<'a, Self::Ast, Self::Token> {
-        self.inner.parse(tokens)
+    fn parse<'b>(&self, tokens: &'b [Self::Token]) -> ParseOutput<'b, Self::Ast, Self::Token> {
+        (self.inner)().parse(tokens)
     }
 
     fn check_left_recursion(&self, depth: usize) -> LeftRecursionCheck {
         if depth == 0 {
             return LeftRecursionCheck::NotOk(vec![]);
         }
-        self.inner.check_left_recursion(depth - 1)
+        (self.inner)().check_left_recursion(depth - 1)
     }
 }
 
-pub(super) fn lazy<'a, T: TokenBounds, A: AstBounds>(
-    f: fn() -> Parser<'a, T, A>,
-) -> LazyParser<'a, T, A> {
+pub(super) fn lazy<'a, T: TokenBounds, A: AstBounds, F: Send + Sync + Fn() -> Parser<'a,T,A>>(
+    f: F,
+) -> LazyParser<'a, T, A, F> {
     LazyParser {
-        inner: LazyLock::new(f),
+        inner: f,
     }
 }
